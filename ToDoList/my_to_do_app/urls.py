3b8538fd080276b40d9c54c3c58b683caf1d3ab0
h0n9670 <likefry98@gmail.com>
2020-03-29 22:21:36 +0900

    todolist comp

--- ToDoList/my_to_do_app/urls.py
+++ ToDoList/my_to_do_app/urls.py
@@ -5,6 +5,8 @@ from .import views
 # 사용자에게 보여줄 화면을 처리할 함수를 담고있는 사용자모듈
 
 urlpatterns = [
-    path('',views.index)
+    path('',views.index),
+    path('createTodo/',views.createTodo, name='createTodo'),
+    path('deleteTodo/',views.doneTodo, name='deleteTodo')
 ]
 
--- ToDoList/my_to_do_app/views.py
+++ ToDoList/my_to_do_app/views.py
@@ -1,8 +1,23 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-# 단순히 ㅇ니자로 받은 문자열을 사용자의 화면에 보여 주도록 하는 함수
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from .models import *
 
 
-# Create your views here
 def index(request):
-    return HttpResponse("my_to_do_app first page")
+    todos = Todo.objects.all()
+    content = {'todos': todos}
+    return render(request, 'my_to_do_app/index.html',content)
+
+def createTodo(request):
+    user_input_str = request.POST['todoContent']
+    new_todo = Todo(content = user_input_str)
+    new_todo.save()
+    return HttpResponseRedirect(reverse(index))
+    #return HttpResponse("create Todo를 할 거야! =>"+user_input_str)
+
+def doneTodo(request):
+    done_todo_id = request.GET['todoNum']
+    todo = Todo.objects.get(id = done_todo_id)
+    todo.delete()
+    return HttpResponseRedirect(reverse(index))
