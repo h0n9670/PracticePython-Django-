74483147f336b8ec9c848bd24ca7750bcb7fa0c2
h0n9670 <likefry98@gmail.com>
2020-04-05 22:47:10 +0900

    comp

--- RestaurantShare/shareRes/models.py
+++ RestaurantShare/shareRes/models.py
@@ -3,3 +3,17 @@ from django.db import models
 # Create your models here.
 class Category(models.Model):
     category_name = models.CharField(max_length = 100)
+
+class Restaurant(models.Model):
+    category = models.ForeignKey(Category, on_delete = models.SET_DEFAULT, default=4)
+    # ForeinKey 외부키를 시용하겠다는 뜻  바로 뒤에 나오는 Category를 참과겠다는 뜻
+    # on_delete는 참조하는 모델이 삭제되었을때 어떻게 할것인 지 물어보는것
+    # on_delete의 종류 : CASCADE 참조하는 요소가 삭제되면 이를 참조하는 다른 요소도 다 삭제
+    #                   PROTECT 참조하는 요소를 삭제할때 오류를 발생
+    #                   SET_NULL 참조하는 요소를 이를 삭제할때 참조하는 다른 요소를 NULL로 만듬
+    #                   SET_DEAFAULT 참조하는 요소를 이를 삭제할때 참조하는 다른 요소들을 DEAFULT 값으로 설정
+    # 아까 지정해둔 기본 기룹의 id값인 4를 default로 설정
+    restaurant_name = models.CharField(max_length = 100)
+    restaurant_link = models.CharField(max_length = 500)
+    restaurant_content = models.TextField()
+    restaurant_keyword = models.CharField(max_length = 50)
--- RestaurantShare/shareRes/views.py
+++ RestaurantShare/shareRes/views.py
@@ -8,22 +8,75 @@ from .models import *
 def index(request):
     categories = Category.objects.all()
     # categories에 Category에 담긴 전체를 받아서 저장하고
-    content = {'categories': categories}
+    restaurants = Restaurant.objects.all()
+    content = {'categories': categories, 'restaurants':restaurants}
     # 딕셔너리 형태로 key값을 할당하여
     return render(request, 'shareRes/index.html', content)
     #render를 통해 index에 전달
 
-def restaurantDetail(request):
+def restaurantDetail(request, res_id):
+    restaurant = Restaurant.objects.get(id = res_id)
+    content = {'restaurant':restaurant}
     #return HttpResponse("restaurantDetail")
-    return render(request, 'shareRes/restaurantDetail.html')
+    return render(request, 'shareRes/restaurantDetail.html',content)
 
 def restaurantCreate(request):
+    categories = Category.objects.all()
+    content = {'categories' : categories}
     #return HttpResponse("restaurantCreate")
-    return render(request, 'shareRes/restaurantCreate.html')
+    return render(request, 'shareRes/restaurantCreate.html', content)
+
+def restaurantUpdate(request, res_id):
+    categories = Category.objects.all()
+    restaurant = Restaurant.objects.get(id = res_id)
+    content = {'categories':categories, 'restaurant':restaurant}
+    return render(request, 'shareRes/restaurantUpdate.html', content)
+
+def Update_restaurant(request):
+    resId = request.POST['resId']
+    change_category_id = request.POST['resCategory']
+    change_category = Category.objects.get(id = change_category_id)
+    change_name = request.POST['resTitle']
+    change_link = request.POST['resLink']
+    change_content = request.POST['resContent']
+    change_keyword = request.POST['resLoc']
+    
+    before_restaurant = Restaurant.objects.get(id = resId)
+    before_restaurant.category = change_category
+    before_restaurant.restaurant_name = change_name
+    before_restaurant.restaurant_link = change_link
+    before_restaurant.restaurant_content = change_content
+    before_restaurant.restaurant_keyword = change_keyword
+    before_restaurant.save()
+    
+    return HttpResponseRedirect(reverse('resDetailPage', kwargs={'res_id':resId}))
+
+def Delete_restaurant(request):
+    res_id = request.POST['resId']
+    restaurant = Restaurant.objects.get(id = res_id)
+    restaurant.delete()
+    return HttpResponseRedirect(reverse('index'))
+
+def Create_restaurant(request):
+    category_id = request.POST['resCategory']
+    category = Category.objects.get(id = category_id)
+    name = request.POST['resTitle']
+    link = request.POST['resLink']
+    content = request.POST['resContent']
+    keyword = request.POST['resLoc']
+    new_res = Restaurant(category=category,restaurant_name=name
+                        ,restaurant_link=link , restaurant_content = content, 
+                        restaurant_keyword=keyword)
+    new_res.save()
+    return HttpResponseRedirect(reverse('index'))
 
 def categoryCreate(request):
-    #return HttpResponse("categoryCreate")
-    return render(request, 'shareRes/categoryCreate.html')
+    categories = Category.objects.all()
+    # categories에 Category에 담긴 전체를 받아서 저장하고
+    content = {'categories': categories}
+    # 딕셔너리 형태로 key값을 할당하여
+    return render(request, 'shareRes/categoryCreate.html',content)
+    #render를 통해 categoryCreate.html에 전달
 
 def Create_category(request):
     category_name = request.POST['categoryName']
@@ -34,3 +87,10 @@ def Create_category(request):
     #데이터베이스에 save함수를 이용하여 저장
     return HttpResponseRedirect(reverse('index'))
     #다시 inedx 화면으로 돌아가도록 리턴해준다.
+
+def Delete_category(request):
+    category_id = request.POST['categoryId']
+    delete_category = Category.objects.get(id = caegory_id)
+    delete_category.delete()
+    return HttpResponseRedirect(reverse('cateCreatePage'))
+
